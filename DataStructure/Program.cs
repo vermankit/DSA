2f7135726b916351173ffa6b80b2ed115b50bb4c
Ankit Verma <a.verma.c@tfoco.com>
2024-09-24 10:47:38 +0530

    (feat) : Reverse a integer.

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             //Console.WriteLine(NumberConverter.ConvertToBinary(11));
+
         }
     }
 
---
+++ DataStructure/ReverseInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal static class ReverseInteger
+    {
+        public static int Reverse(int x)
+        {
+            int reverseNumber = 0;
+            while (x != 0)
+            {
+                var lastdigit = x % 10;
+                if (reverseNumber < int.MinValue / 10
+                || reverseNumber > int.MaxValue / 10)
+                {
+                    return 0;
+                }
+                reverseNumber = reverseNumber * 10 + lastdigit;
+                x = x / 10;
+            }
+
+            return reverseNumber;
+        }
+    }
+}
