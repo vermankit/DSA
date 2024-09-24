f6998165f8342de69df6533ceaebe32374a6237d
Ankit Verma <a.verma.c@tfoco.com>
2024-09-24 10:47:38 +0530

    (feat) : Reverse a integer.

---
+++ DataStructure/PowerOfTwocs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class PowerOfTwocs
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+            while (n != 0)
+            {
+                var lastbit = n & 1;
+                n = n >> 1;
+                if (lastbit == 1 && n != 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
